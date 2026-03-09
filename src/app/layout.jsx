993cb9119d285725324f8d8e5a257ed1b9ad7ad5
the-abed <mohammadabed1283@gmail.com>
2026-03-09 23:37:11 +0600

    update with next auth provider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,22 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import NextAuthProvider from "@/provider/NextAuthProvider";
+import Navbar from "@/components/Navbar";
 const poppins = Poppins({
   weight: ["200", "400", "500", "600", "700"],
 });
 
 const RootLayout = ({ children }) => {
   return (
-    <html className={`${poppins.className}`}>
-      <body className="">{children}</body>
-    </html>
+    <NextAuthProvider>
+      <html className={`${poppins.className}`}>
+        <body className="">
+          <Navbar></Navbar>
+          {children}
+        </body>
+      </html>
+    </NextAuthProvider>
   );
 };
 export default RootLayout;
